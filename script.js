161196a99079573512d3e9df6b11c3d433aff3b8
PROJWEB batumeni <Bajo2204@student.iepscf-uccle.be>
2020-10-26 23:55:50 +0100

    Notfinish

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Écrivez votre code JavaScript ici.
+const list = document.querySelector('#ul');
+      const input = document.querySelector('input');
+      const button = document.querySelector('button');
+
+      button.onclick =  btn () => {
+
+        input.value =""
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+
+
+        listItem.appendChild(listBtn)
+        listText.textContent = toBuy
+        listItem.appendChild(listText)
+listBtn.textContent ="erase"
+
+
+
+      }
